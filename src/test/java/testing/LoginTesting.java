47ed792b903641afa37423b35d1ae69f14992681
Erazz55 <52203445+Erazz55@users.noreply.github.com>
2022-07-29 11:42:34 +0530

    orange login test case

--- src/test/java/testing/LoginTesting.java
+++ src/test/java/testing/LoginTesting.java
@@ -5,23 +5,55 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LoginTesting {
 
-	public static void main(String[] args) {
-	
-		WebDriver driver = new FirefoxDriver();
-//		driver.get("http://google.com");
-//		driver.quit();
-		
+	private WebDriver driver;
+	private Map<String, Object> vars;
+	JavascriptExecutor js;
+
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+		js = (JavascriptExecutor) driver;
+		vars = new HashMap<String, Object>();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void orangeHRMLoginTestCase() {
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-	    driver.manage().window().setSize(new Dimension(1594, 860));
-	    driver.findElement(By.cssSelector("#divUsername > .form-hint")).click();
-	    driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-	    driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-	    driver.findElement(By.id("btnLogin")).click();
-	    driver.findElement(By.id("welcome")).click();
-	    driver.findElement(By.linkText("Logout")).click();
-	    driver.quit();
+		driver.manage().window().setSize(new Dimension(1594, 860));
+		driver.findElement(By.cssSelector("#divUsername > .form-hint")).click();
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		driver.findElement(By.id("welcome")).click();
+		driver.findElement(By.linkText("Logout")).click();
 	}
 
 }
