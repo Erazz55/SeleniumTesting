ae1d450c2c551fb81ecf60dff1160929a9fcf554
Erazz55 <52203445+Erazz55@users.noreply.github.com>
2022-07-29 15:29:01 +0530

    edited

--- src/test/java/testing/LoginTesting.java
+++ src/test/java/testing/LoginTesting.java
@@ -50,12 +50,7 @@ public class LoginTesting {
 		
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().setSize(new Dimension(1594, 860));
-		driver.findElement(By.cssSelector("#divUsername > .form-hint")).click();
-		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-		driver.findElement(By.id("btnLogin")).click();
-//		driver.findElement(By.id("welcome")).click();
-//		driver.findElement(By.linkText("Logout")).click();
+		
 	}
 
 }
